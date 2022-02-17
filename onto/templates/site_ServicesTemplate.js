ba4d7f660e82028ef580aa0e64b7836f9991b3eb
Roman Karpov <roman.karpov@gmail.com>
2022-02-17 10:33:58 +0300

    fix templates

--- onto/templates/site_ServicesTemplate.js
+++ onto/templates/site_ServicesTemplate.js
@@ -2,10 +2,8 @@ export const html = `
   <div about="@" rel="site:hasArticle">
     <div class="container sheet page-sheet">
       <div class="row">
-        <div class="col-md-2 col-md-push-10 vcenter" about="@" rel="v-s:hasImage" data-template="v-ui:ImageTemplate"></div>
-        <!--
-   -->
-        <div class="col-md-10 col-md-pull-2 vcenter">
+        <div class="col-md-2 col-md-push-10 vcenter" about="@" rel="v-s:hasImage" data-template="v-ui:ImageTemplate"></div><!--
+     --><div class="col-md-10 col-md-pull-2 vcenter">
           <h1 about="@" property="site:heading"></h1>
           <p about="@" property="site:summary" class="markdown lead"></p>
         </div>
